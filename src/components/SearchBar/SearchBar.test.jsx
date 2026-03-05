04dd0b06353624b98ff9e52cf907871f75c9951a
MaPankow <162994783+MaPankow@users.noreply.github.com>
2026-03-05 18:12:52 +0100

    Strukturiere SearchBar.test.jsx um, sodass eine Vorbereitungsfunktion wiederkehrende Aufgaben übernimmt.

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -4,24 +4,27 @@ import { userEvent } from '@testing-library/user-event';
 
 
 describe('Search Bar component', () => {
-    it('renders search bar correctly', () => {
-        const mockSearchSpotify = vi.fn();
-        
-        render(<SearchBar searchSpotify={mockSearchSpotify} />);
+    let mockSearchSpotify;
+    let user;
 
+    beforeEach(() => {
+        mockSearchSpotify = vi.fn();
+        render(<SearchBar searchSpotify={mockSearchSpotify} />);
+        user = userEvent.setup();        
+    });
+
+    it('renders search bar correctly', () => {
+              
         expect(screen.getByText("Find your favourite tracks and add them to the playlist")).toBeInTheDocument();
         expect(screen.getByLabelText("Browse tracks:")).toBeInTheDocument();
         expect(screen.getByRole("button", { name: /search/i })).toBeInTheDocument();
     });
 
     it('allows user to type into the input field', async () => {
-        const mockSearchSpotify = vi.fn();
+       
 
-        render(<SearchBar searchSpotify={mockSearchSpotify} />);
-        const user = userEvent.setup();
         const input = screen.getByLabelText(/browse tracks:/i);
         await user.type(input, "Muse");
-
         expect(input).toHaveValue("Muse");
     });
     
