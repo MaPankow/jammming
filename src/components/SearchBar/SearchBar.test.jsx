b66c00a1d2de26047e04289ef927f8869e79656b
MaPankow <162994783+MaPankow@users.noreply.github.com>
2026-03-06 13:13:14 +0100

    Strukturiere wieder um, um durch eine setup()-Funktion Wiederholungen im Code zu sparen

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -2,35 +2,40 @@ import SearchBar from "./SearchBar";
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
+const setup = () => {
+    const user = userEvent.setup()
+    const mockSearchSpotify = vi.fn();
 
-describe('Search Bar component', () => {
-    let mockSearchSpotify;
-    let user;
+    render(<SearchBar searchSpotify={mockSearchSpotify} />);
+    
+    return {
+        user,
+        mockSearchSpotify,
+        input: screen.getByLabelText(/browse tracks/i),
+        button: screen.getByRole("button", { name: /search/i })
+    }; 
+};
 
-    beforeEach(() => {
-        mockSearchSpotify = vi.fn();
-        render(<SearchBar searchSpotify={mockSearchSpotify} />);
-        user = userEvent.setup();        
-    });
 
-    it('renders search bar correctly', () => {
-              
+describe("Search Bar component", () => {
+    it("renders search bar correctly", () => {
+        render(<SearchBar searchSpotify={vi.fn()} />);
+
         expect(screen.getByText("Find your favourite tracks and add them to the playlist")).toBeInTheDocument();
         expect(screen.getByLabelText("Browse tracks:")).toBeInTheDocument();
         expect(screen.getByRole("button", { name: /search/i })).toBeInTheDocument();
     });
 
     it('allows user to type into the input field', async () => {
-       
+        const { user, input } = setup();
 
-        const input = screen.getByLabelText(/browse tracks:/i);
         await user.type(input, "Muse");
+        
         expect(input).toHaveValue("Muse");
     });
     
     it('calls searchSpotify when form is submitted', async () => {
-        const input = screen.getByLabelText(/browse tracks:/i);
-        const button = screen.getByRole("button", { name: /search/i });
+        const { user, input, button, mockSearchSpotify } = setup();
         
         await user.type(input, "Muse");
         await user.click(button);
